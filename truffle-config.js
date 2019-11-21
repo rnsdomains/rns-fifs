5acbc36a3f8e6013e9594bd7dc2234cd32eb62e6
Ilan Olkies <ilanolkies@outlook.com>
2019-11-20 23:42:24 -0300

    Add HD derivation

--- truffle-config.js
+++ truffle-config.js
@@ -16,7 +16,7 @@ module.exports = {
       gasPrice: 6000000000,
     },
     mainnet: {
-      provider: () => new HDWalletProvider(mnemonic, 'https://public-node.rsk.co'),
+      provider: () => new HDWalletProvider(mnemonic, 'https://public-node.rsk.co', 0, 1, true, `m/44'/137'/0'/0/`),
       network_id: 30,
       gasPrice: 60000000,
     },
